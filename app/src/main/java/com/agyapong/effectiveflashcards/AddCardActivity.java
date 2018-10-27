0b9c994c021c69bde7cd2ac3d7af6dc33c633006
user <agyapongeli77@gmail.com>
2018-10-27 12:10:25 -0400

    Updated to lab 2 required challenges 1. Added a ‘+’ button that takes the user to new ‘Add Card Screen’ 2. Added a Cancel button to the 'Add Card Screen' 3. Added two fields to the 'Add Card Screen' that will allow the user to enter the Question and the Answer 4. Added a Save button that dismisses 'Add Card Screen' and returns to the Main Activity where the user can see the card they just created

---
+++ app/src/main/java/com/agyapong/effectiveflashcards/AddCardActivity.java
@@ -0,0 +1,42 @@
+package com.agyapong.effectiveflashcards;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        // dismisses or closes the AddCardActivity(Add Card Screen) and takes the user back
+        // to the Main Activitity(Main Screen) when the cancel button is clicked by the user
+        findViewById(R.id.cancel_action_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // collect user input and pass it back to the Main Activity to be displayed when the save button is clicked
+        findViewById(R.id.save_data_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String question = ((EditText) findViewById(R.id.questionTextField)).getText().toString();
+                String answer = ((EditText) findViewById(R.id.questionTextField)).getText().toString();
+
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                data.putExtra("string1", question); // puts the question data collected from the user into the Intent, with the key as 'string1'
+                data.putExtra("string2", answer); // puts the answer data collected from the user into the Intent, with the key as 'string2
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/agyapong/effectiveflashcards/MainActivity.java
+++ app/src/main/java/com/agyapong/effectiveflashcards/MainActivity.java
@@ -1,9 +1,11 @@
 package com.agyapong.effectiveflashcards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     boolean isShowingAnswers = false;
@@ -13,21 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this is a wrong answer so set the background to RED when user clicks this option
+        // this is a wrong answer so set the background to RED when user clicks this option
         findViewById(R.id.multiple_choice_GitLab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.multiple_choice_GitLab).setBackgroundColor(getResources().getColor(R.color.my_red_color));
             }
         });
-        ////this is the correct answer so set the background to GREEN when user clicks this option
+        // this is the correct answer so set the background to GREEN when user clicks this option
         findViewById(R.id.multiple_choice_GitHub).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.multiple_choice_GitHub).setBackgroundColor(getResources().getColor(R.color.my_green_color));
             }
         });
-        //this is a wrong answer so set the background to RED when user clicks this option
+        // this is a wrong answer so set the background to RED when user clicks this option
         findViewById(R.id.multiple_choice_Bitbucket).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Reset the multiple choice colors back to it's original state when user clicks the rootview
+        // reset the multiple choice colors back to it's original state when user clicks the rootview
         findViewById(R.id.rootview).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //toggle between showing the answers and hiding the answers when user clicks the hide/show "eye" button
+        // toggle between showing the answers and hiding the answers when user clicks the hide/show "eye" button
         findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -66,10 +68,42 @@ public class MainActivity extends AppCompatActivity {
                     ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.show_icon);
                     isShowingAnswers = false;
                 }
+            }
+        });
+
+        // takes user to the AddCardActivity(Add Card Screen) when the add icon is clicked from the Main Activity(Main Screen)
+        findViewById(R.id.add_card_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                // startActivityForResult specifies that we want result(data) to be returned back to the Main Activity from the AddCardActivity
+                MainActivity.this.startActivityForResult(intent, 100);
 
             }
         });
 
 
+    }
+
+    // this method is responsible for getting the data passed from the AddCardActivity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+
+            // sets the question and answer TextViews to the new question and answer data that user
+            // inputted at the AddCardActivity and was passed to the main activity
+            ((TextView)findViewById(R.id.flashcard_question)).setText(string1);
+            ((TextView)findViewById(R.id.multiple_choice_GitHub)).setText(string2);
+
+
+            // Hide the other multiple choice answers since they are not in the Required challenge
+            // Add it when you are working on the Optional challenge later on
+            findViewById(R.id.multiple_choice_GitLab).setVisibility(View.INVISIBLE);
+            findViewById(R.id.multiple_choice_Bitbucket).setVisibility(View.INVISIBLE);
+
+
+        }
     }
 }
